d3baf047d4c214a7faa954685774ee05c738c234
Musel25 <muselemmanuel@hotmail.com>
2025-08-13 00:56:32 -0700

    footer-copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <small>&copy; Copyright by ByteGrad. All rights reserved.</small>
+      <small>&copy; Copyright by MuselTabares. All rights reserved.</small>
       <small>Last checked limits: 17 days ago</small>
     </footer>
   );
